a1519752939c9607654db462e1bc474eb90d4999
roshansabale@yahoo.com <roshansabale@yahoo.com>
2021-04-20 23:08:37 +0530

    Refactored: code to return if employee absent

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,11 +1,15 @@
 //UC 1
-const IS_ABSENT = 0;
-let empCheck = Math.floor(Math.random() * 10) % 2;
-console.log(empCheck);
-if (empCheck == IS_ABSENT)
-    console.log("Employee is Abscent");
-else
-    console.log("Employee is Present");
+{
+    const IS_ABSENT = 0;
+    let empCheck = Math.floor(Math.random() * 10) % 2;
+    console.log(empCheck);
+    if (empCheck == IS_ABSENT) {
+        console.log("UC 1: Employee is Abscent Exiting the program");
+        return;
+    } else {
+        console.log("Employee is Present");
+    }
+}
 
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -27,4 +31,4 @@ switch (empCheck) {
 }
 
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Employee wage :"+empWage);
+console.log("Employee wage :" + empWage);
