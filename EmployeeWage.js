356fdd07dac699033bf746e73c1092f0ef7821c9
roshansabale@yahoo.com <roshansabale@yahoo.com>
2021-04-21 20:25:36 +0530

    Added: code to calculate the wage for total working days

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -17,7 +17,7 @@ const PART_TIME_HOUR = 4;
 const FULL_TIME_HOUR = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
-
+const MAX_HRS_IN_MONTH = 160;
 
 
 function getWorkingHours(empCheck) {
@@ -31,11 +31,13 @@ function getWorkingHours(empCheck) {
     }
 }
 
-let empHrs = 0;
 
-for (let day = 0; day <= NUM_OF_WORKING_DAYS; day++) {
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += getWorkingHours(empCheck);
 }
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total Hrs: " + empHrs + " Employee wage:  " + empWage);
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total Days: " + totalWorkingDays + " Total Hrs: " + totalEmpHrs + " Employee wage:  " + empWage);
