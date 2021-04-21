1ae2f1ca194eeec028bae29c7330767be2a8988a
roshansabale@yahoo.com <roshansabale@yahoo.com>
2021-04-21 20:10:40 +0530

    Added: code to caluculate total working hours according to it calculate wgae

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -16,6 +16,7 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOUR = 4;
 const FULL_TIME_HOUR = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 
 
@@ -30,7 +31,11 @@ function getWorkingHours(empCheck) {
     }
 }
 
-let empCheck = Math.floor(Math.random() * 10) % 3;
-let empHrs = getWorkingHours(empCheck);
+let empHrs = 0;
+
+for (let day = 0; day <= NUM_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs = getWorkingHours(empCheck);
+}
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Employee wage :" + empWage);
+console.log("Total Hrs: " + empHrs + " Employee wage:  " + empWage);
