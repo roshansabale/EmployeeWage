3c943f33019488765e6394196dc9ad9ec2c0c2f3
roshansabale@yahoo.com <roshansabale@yahoo.com>
2021-04-25 11:37:03 +0530

    Added: code to create EmployeePayroll class and insert data using object constructor

--- EmployeePayrollDataClass.js
+++ EmployeePayrollDataClass.js
@@ -5,11 +5,20 @@ class EmployeePayrollData {
     gender;
     startDate;
 
-    //Constructor
-    constructor(id, name, salary) {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+    //Constructor using normal 
+    // constructor(id, name, salary) {
+    //     this.id = id;
+    //     this.name = name;
+    //     this.salary = salary;
+    // }
+
+    //constructor using spread operator which set value as per provided values
+    constructor(...params) {
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
 
     //Getter and setter
@@ -21,7 +30,11 @@ class EmployeePayrollData {
 
     //method
     toString() {
-        return "id= " + this.id + ", name= " + this.name + ", salary= " + this.salary;
+        const option = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = this.startDate === undefined ? "undefined" :
+            this.startDate.toLocaleDateString("en-US", option);
+        return "id= " + this.id + ", name= " + this.name + ", salary= " + this.salary + ", Gender: " +
+            this.gender + ", Start Date: " + empDate;
     }
 }
 
@@ -30,3 +43,5 @@ console.log(employeePayrollData.toString());
 employeePayrollData.id = 200;
 employeePayrollData.name = "Roshan";
 console.log(employeePayrollData.toString());
+let employeePayrollData1 = new EmployeePayrollData(100, "Joffera", 350000, "Male", new Date());
+console.log(employeePayrollData1.toString());
